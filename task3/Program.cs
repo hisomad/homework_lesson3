8c2c2c14f56acb66600e74887dd21c300cd75964
Vika <vika.tulnikova@bk.ru>
2022-12-29 14:43:57 +0300

    add task3

---
+++ task3/Program.cs
@@ -0,0 +1,12 @@
+﻿string TableСube(int n)
+{
+    string answer = string.Empty;
+
+    for (int i = 1; i <= n; i++)
+        answer += (i == n)? Math.Pow(i,3).ToString() : Math.Pow(i,3).ToString() + ",";
+    
+    return answer;
+}
+
+int n = 5;
+System.Console.WriteLine(TableСube(n));
